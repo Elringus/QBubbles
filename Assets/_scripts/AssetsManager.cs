49c4453f87edb6184c8350da8702572c5af35731
Elringus <elringus@yandex.ru>
2014-05-30 12:58:13 +0400

    added asset bundle streaming
    downloads (locally) background prefab at runtime

---
+++ Assets/_scripts/AssetsManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AssetsManager : MonoBehaviour
+{
+	private void Start () 
+	{
+		// load background from asset bundle 
+		StartCoroutine(DownloadAsset("file://" + Application.dataPath + "/AssetBundles/asb_Background.unity3d", "background"));
+	}
+
+	private IEnumerator DownloadAsset (string url, string name)
+	{
+		// download the file from the URL
+		using (WWW www = new WWW(url))
+		{
+			yield return www;
+			if (www.error != null)
+				throw new Exception("WWW download had an error:" + www.error);
+
+			// load and retrieve the AssetBundle
+			AssetBundle bundle = www.assetBundle;
+
+			// load the object asynchronously
+			AssetBundleRequest request = bundle.LoadAsync(name, typeof(GameObject));
+			yield return request;
+
+			// instantiate the object
+			GameObject obj = request.asset as GameObject;
+			GameObject.Instantiate(obj);
+
+			// Unload the AssetBundles compressed contents to conserve memory
+			bundle.Unload(false);
+		} 
+	}
+}
---
+++ Assets/_scripts/Background.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour
+{
+	private void Awake () 
+	{
+		// scaling sprite so it will cover the entire screen
+		SpriteRenderer spr = renderer as SpriteRenderer;
+		float diff = Screen.height / spr.sprite.textureRect.height * 1.5f;
+		transform.localScale = new Vector3(diff, diff, 1);
+	}
+
+}
